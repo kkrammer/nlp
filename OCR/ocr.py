907048a21704b39b077720ad0745e2dcd15e7a39
kkrammer <56035402+kkrammer@users.noreply.github.com>
2023-12-22 08:59:12 -0500

    comm

---
+++ OCR/ocr.py
@@ -0,0 +1,8 @@
+import cv2 as cv 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import tensorflow as tf 
+
+mnist = tf.keras.datasets.mnist 
+(x_train, y_train),(x_test,y_test) = mnist.load_data() 
+
